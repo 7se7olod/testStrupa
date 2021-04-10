8d564b345b5abe02317dd99254b7512c07a63ac3
7se7olod <pushin_95@mail.ru>
2021-04-10 13:31:57 +0400

    Initial Commit

---
+++ testStrupa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testStrupa
+//
+//  Created by Всеволод on 10.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
